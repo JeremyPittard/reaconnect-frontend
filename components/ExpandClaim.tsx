a4ceba401c12b212335602919e958a1c0117f380
Jeremy Pittard <jeremy@jpittard.net>
2023-05-02 08:06:41 +0800

    added waitlist to expanded claim component

--- components/ExpandClaim.tsx
+++ components/ExpandClaim.tsx
@@ -1,23 +1,26 @@
+import WaitlistForm from "./WaitlistForm";
+
 const ExpandClaim = () => {
   return (
-    <section className="mt-40 max-w-3xl mx-auto">
-      <h1 className="text-3xl">
+    <section className="my-40 max-w-3xl mx-auto">
+      <h1 className="text-5xl mb-5">
         {`
-        At Copy Ipsum, we believe that many real estate assets are "always for
-        sale" or "always on the market" for the right price, even if they aren't
-        listed publicly.
+        At ReaConnect, we believe that many real estate assets are "always for
+        sale"... 
       `}
       </h1>
-      <p>
-        {`
+      <p className="mb-7">
+        {`...or "always on the market" for the right price, even if they aren't
+        listed publicly.
         That's why we're building a platform to connect potential buyers and
         renters with homeowners who haven't yet listed their property for sale.
         With our platform, you can make an offer on the house or apartment of
         your dreams, even if it's not officially on the market. Say goodbye to
         endless hours of scrolling through listings and hello to your perfect
-        home or rental property. Join Copy Ipsum today and start your search!
+        home or rental property. Sign up now for updates and launch information!
         `}
       </p>
+      <WaitlistForm />
     </section>
   );
 };
