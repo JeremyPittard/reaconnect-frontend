ea32417f5336bce66c2d44d5384822329f0071a3
Jeremy Pittard <jeremy@jpittard.net>
2023-05-11 14:03:57 +0800

    footer update

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const PageFooter = () => {
   return (
     <footer>
-      <nav className="max-w-md border-t border-storm-green mx-auto text-right">
-        <ul className="flex gap-4">
+      <nav className="max-w-7xl border-t border-storm-green mx-auto text-right">
+        <ul className="flex gap-4 items-end py-2 mx-auto max-w-md">
+          <li>
+            <Link href={"/"}>
+              <Image
+                src="/img/logo.svg"
+                alt="reaconnect logo"
+                height={35}
+                width={35}
+              />
+            </Link>
+          </li>
           <li>
             <Link href={"/investors"}>Investors</Link>
+          </li>
+          <li>
+            <Link href={"/investors"}>Service Providers</Link>
           </li>
         </ul>
       </nav>
